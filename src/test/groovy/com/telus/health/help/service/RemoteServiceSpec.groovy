590804f707f689c290c6f21c7110d5751e51605b
Martin Zardecki <99494656+calgarymarty@users.noreply.github.com>
2023-01-25 16:32:50 -0700

    SREF-1 #comment added new spec method #time 1h

--- src/test/groovy/com/telus/health/help/service/RemoteServiceSpec.groovy
+++ src/test/groovy/com/telus/health/help/service/RemoteServiceSpec.groovy
@@ -1,13 +1,17 @@
 package com.telus.health.help.service
 
 import org.springframework.boot.test.context.SpringBootTest
+import com.telus.health.claim.service.RemoteService
 import com.telus.health.help.SpockRefApplication
 import spock.lang.Specification
 
 @SpringBootTest(classes = SpockRefApplication.class, webEnvironment =  SpringBootTest.WebEnvironment.MOCK)
 class RemoteServiceSpec extends Specification {
+
+  def service = Mock(RemoteService)
+
   def newTest()
   {
-
+    service.executeRemoteAction( "data" )
   }
 }
